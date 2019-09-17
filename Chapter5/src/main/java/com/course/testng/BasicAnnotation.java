f4f69e8e451d4dcc6f6d2c791a444eb84e221947
Administrator <543839989@qq.com>
2019-09-17 13:05:37 +0800

    tijiao test

--- Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
+++ Chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -4,6 +4,6 @@ import org.testng.annotations.Test;
 public class BasicAnnotation {
     @Test
     public void testCase1(){
-        System.out.println("只是测试用例1");
+        System.out.println("只是测试用例12322222");
     }
 }
